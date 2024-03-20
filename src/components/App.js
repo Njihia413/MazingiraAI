35189e42f6104961408877ab67c728975ce0e136
jameskanyiri <jmskanyiri@gmail.com>
2024-03-21 00:06:00 +0300

    started chat Ui

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import {Route, Routes} from "react-router-dom";
 import Home from "./Home";
 import Login from "./Login";
 import Signup from "./Signup";
+import Chat from "./Chat";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
             <Route path="/" element={<Home/>}/>
             <Route path="/login" element={<Login/>}/>
             <Route path="/signup" element={<Signup/>}/>
+            <Route path="/chat" element={<Chat/>}/>
         </Routes>
     </div>
   );
