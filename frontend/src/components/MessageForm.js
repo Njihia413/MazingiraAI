170b9233019b8f86fd0dae4b683303f7ba12f2a9
Vincent <makokhavomondi@gmail.com>
2024-05-11 22:15:04 +0300

    Clear message form when submitting a message

--- frontend/src/components/MessageForm.js
+++ frontend/src/components/MessageForm.js
@@ -14,6 +14,7 @@ export default function MessageForm({sendPrompt}){
     function handleSubmit(e){
         e.preventDefault()
         sendPrompt(prompt)
+        setPrompt('')
     }
 
     return (
