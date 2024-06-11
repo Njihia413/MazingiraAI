45424236a4e8372bca23c8a849fb4290676c0a8f
Vincent <makokhavomondi@gmail.com>
2024-06-11 23:57:03 +0300

    Get leaflet to work for Kenya

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -4,12 +4,11 @@ import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
 
 function Map(){
     return (
-        <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <MapContainer center={[-0.023559, 37.906193]} zoom={7} scrollWheelZoom={false} style={{height: '100vh'}}>
             <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={[51.505, -0.09]}>
+            <Marker position={[-0.023559, 37.906193]}>
                 <Popup>
                     A pretty CSS3 popup. <br /> Easily customizable.
                 </Popup>
